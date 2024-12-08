41c034a8a977791ca67a12c795e04309245936a3
Rlezzo <86238734+Rlezzo@users.noreply.github.com>
2024-12-09 04:55:36 +0800

    Delete backend directory

--- backend/database/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# backend/database/models
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-# 导入所有模型以便于外部可以访问
-from .rss_feed import RSSFeed
-from .task import Magnet
--- backend/views/__init__.py
+++
@@ -1,5 +0,0 @@
-# backend/views
-from .rss import rss_blueprint
-from .log import log_blueprint
-from .auth import auth_blueprint
-from .magnet import magnet_blueprint
