7f43d7aaf19539689f643e302144a25cd9418a97
Rlezzo <86238734+Rlezzo@users.noreply.github.com>
2024-12-09 23:55:30 +0800

    Delete AniAlist directory

--- AniAlist/generate_hashed_password.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-# 生成固定哈希值
-username = "admin"
-password = "mysecurepassword"
-hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-print(f"Username: {username}")
-print(f"Hashed Password: {hashed_password.decode()}")
